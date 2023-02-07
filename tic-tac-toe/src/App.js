dd50cb27d0cbb921ff7a68ba1da7a1d53e659962
Agnita <ageetha2@pdx.edu>
2023-02-07 11:28:26 -0800

    updated board function

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,39 +1,6 @@
 import React from 'react';
 import './App.css';
 
-function calculateWinner(squares)
-{
-  const lines = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
-  for(let i=0;i<lines.length;i++)
-  {
-    const [a,b,c]=lines[i]
-    if( squares[a] && squares[a] === squares[b] && squares[a] === squares[c] )
-    {
-      return a
-    }   
-
-  }
-  return null
-}
-
-
-function calculateNextValue(squares) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'Z'
-}
-
-function calculateStatus(winner,squares,nextValue) {
-  return winner ? `Winner: ${winner}` : squares.every(Boolean) ? `Scratch: Cat's game` : `Next player: ${nextValue}`
-}
-
 function Board() {
   const [squares,setSquares] = React.useState(Array(9).fill(null))
 
@@ -90,14 +57,53 @@ function Board() {
   )
 }
 
+function Game() 
+{
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+     </div>
+    </div>
+  )
+}
 
+function calculateStatus(winner,squares,nextValue) {
+  return winner ? `Winner: ${winner}` : squares.every(Boolean) ? `Scratch: Cat's game` : `Next player: ${nextValue}`
+}
+
+function calculateNextValue(squares) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'Z'
+}
+
+function calculateWinner(squares)
+{
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  for(let i=0;i<lines.length;i++)
+  {
+    const [a,b,c]=lines[i]
+    if( squares[a] && squares[a] === squares[b] && squares[a] === squares[c] )
+    {
+      return squares[a]
+    }   
+
+  }
+  return null
+}
 
 function App() {
-  return (
-    <div className="App">
-      hello react
-    </div>
-  );
+  return <Game />
+    
+  
 }
 
 export default App;
