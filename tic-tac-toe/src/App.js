e2bf6d25921d65dc7d5ce0c2ef8dbbceba6b9a9e
Agnita <ageetha2@pdx.edu>
2023-02-07 11:17:16 -0800

    created board with functions

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -34,6 +34,64 @@ function calculateStatus(winner,squares,nextValue) {
   return winner ? `Winner: ${winner}` : squares.every(Boolean) ? `Scratch: Cat's game` : `Next player: ${nextValue}`
 }
 
+function Board() {
+  const [squares,setSquares] = React.useState(Array(9).fill(null))
+
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
+  function selectSquare(square) 
+  {
+    if (winner || squares[square])
+    {
+      return
+    }
+    const squareCopy = [...squares]
+    squareCopy[square] = nextValue
+    setSquares(squareCopy)
+  }
+
+  function restart()
+  {
+    setSquares(Array(9).fill(null))
+  }
+
+  function renderSquare(i) 
+  {
+    return (
+    <button className="square" onClick={ () => selectSquare(i)}>
+      {squares[i]}
+
+    </button>
+    )
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+      {renderSquare(0)}
+      {renderSquare(1)}
+      {renderSquare(2)}
+      </div>
+      <div className="board-row">
+      {renderSquare(3)}
+      {renderSquare(4)}
+      {renderSquare(5)}
+      </div>
+      <div className="board-row">
+      {renderSquare(6)}
+      {renderSquare(7)}
+      {renderSquare(8)}
+      </div>
+      <button className='restart' onClick={restart}>Restart</button>
+    </div>
+  )
+}
+
+
+
 function App() {
   return (
     <div className="App">
